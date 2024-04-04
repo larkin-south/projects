ca24fc57bc50559646debfc394629ae454d1a53e
Larkin South <160358440+larkin-south@users.noreply.github.com>
2024-04-03 22:10:14 -0400

    Added box columns.

--- etch-sketch/scripts.js
+++ etch-sketch/scripts.js
@@ -1,11 +1,29 @@
 function grid() {
-    for (let i = 0; i < 16; i++) {
-        let box = document.createElement("div");
-        box.classList.add("box");
-        
-        let container = document.getElementsByClassName("container");
-        container[0].appendChild(box);
+    let container = document.getElementsByClassName("container");
+
+    for (let row = 0; row < 16; row++) {
+        let line = document.createElement("div")
+        line.classList.add("container");
+
+        // container[0].appendChild(line);
+
+        for (let col = 0; col < 16; col++) {
+            let box = document.createElement("div");
+            box.classList.add("box");
+            
+            line.appendChild(box);
+            container[0].appendChild(line);
+        }
+
+        // if (row == (row - 1)) {
+        //     break;
+        // } else {
+        //     let line = document.createElement("br");
+        //     container[0].appendChild(line);
+        // }
     }
+
+
 }
 
 grid();
