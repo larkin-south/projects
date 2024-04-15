77e2c3026e063d8210ac1157a401d7edbba0bb8a
Larkin South <160358440+larkin-south@users.noreply.github.com>
2024-04-15 16:43:26 -0500

    Working on adding decimals and rounding values.

--- calculator/scripts.js
+++ calculator/scripts.js
@@ -30,7 +30,11 @@ function operate(a, operator, b) {
 function updateOutput(output) {
     let display = document.getElementById("output");
 
-    display.innerText = output.join("");
+    if (output.length < 2) {
+        display.innerText = output;
+    } else {
+        display.innerText = output.join("");
+    }
 }
 
 function checkValue(e) {
@@ -41,8 +45,12 @@ function checkValue(e) {
         opCount++
         if (opCount == 1) {
             output[1] = newEntry;
+        } else if (opCount == 2 && isNaN(output[2])) {
+            output[1] = newEntry;
+            opCount = 1;
         } else if (opCount == 2) {
             let newCalculation = operate(output[0], output[1], output[2]);
+
             output[0] = newCalculation;
             output[1] = newEntry;
             output[2] = "";
@@ -64,14 +72,35 @@ function checkValue(e) {
             output[2] = output[2] + newEntry;
             }
         }
-    }
+    }    
 
     updateOutput(output);
+}
+
+function clearContents() {
+    output = [0];
+    updateOutput(output);
+    opCount = 0;
+}
+
+function calculateResults() {
+    let regex = /[^0-9]/;
+
+    if (!isNaN(output[0]) && regex.test(output[1]) && !isNaN(output[2])) { 
+        let newCalculation = operate(output[0], output[1], output[2]);
+
+        output[0] = newCalculation;
+        output.splice(1, 2);
+        updateOutput(output);
+        opCount = 1; 
+    }
 }
 
 function activateButtons() {
     let numRows = document.getElementsByClassName("numRow");
     let opCol = document.getElementById("operatorColumn").childNodes;
+    let clear = document.getElementById("clear");
+    let equals = document.getElementById("calculate");
 
     for (element of numRows) {
         let nums = element.getElementsByClassName("number");
@@ -84,8 +113,13 @@ function activateButtons() {
     for (element of opCol) {
         element.addEventListener("click", checkValue);
     }
+
+    clear.addEventListener("click", clearContents);
+    equals.addEventListener("click", calculateResults);
+
+    updateOutput(output);
 }
 
-activateButtons();
 let opCount = 0;
-let output = [];
+let output = [0];
+activateButtons();
