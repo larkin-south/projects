070535a70733f5dccc7371a9bbca92648b414f23
Larkin South <160358440+larkin-south@users.noreply.github.com>
2024-04-23 15:43:16 -0500

    Substrings complete.

--- ruby/substrings.rb
+++ ruby/substrings.rb
@@ -1,7 +1,11 @@
 def substrings(string, dictionary)
+    string = string.split(" ")
+    letters_only = string.map { |item| item.gsub(/[^a-zA-Z]/, "").downcase }
     dictionary.reduce(Hash.new) do |acc, item| 
-        if string.include?(item)
-            if acc[item].nil? ? acc[item] = 1 : acc[item] += 1
+        letters_only.each do |word|
+            if word.include?(item)
+                if acc[item].nil? ? acc[item] = 1 : acc[item] += 1
+                end
             end
         end
         acc
@@ -9,5 +13,5 @@ def substrings(string, dictionary)
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-puts substrings("below", dictionary)
-# puts substrings("Howdy partner, sit down! How's it going?", dictionary)
+# puts substrings("below", dictionary)
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
