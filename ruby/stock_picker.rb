1b5065049870b7d087c0159239a8eff2a2216baf
Larkin South <160358440+larkin-south@users.noreply.github.com>
2024-04-23 17:02:57 -0500

    Started stock_picker.

---
+++ ruby/stock_picker.rb
@@ -0,0 +1,13 @@
+def stock_picker(stock_values)
+
+end
+
+
+
+
+
+stock_picker([17,3,6,9,15,8,6,1,10])
+
+# for each num in the array
+#     slice the array up to the current index
+#     return hash of current num as key with value equal to num subtracted from remaining items
