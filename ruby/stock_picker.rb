944475de43b053b0ee01f8c2b2578d0840574e92
Larkin South <160358440+larkin-south@users.noreply.github.com>
2024-04-23 20:12:52 -0500

    stock-picker complete.

--- ruby/stock_picker.rb
+++ ruby/stock_picker.rb
@@ -1,17 +1,18 @@
 def stock_picker(stock_values)
     stock_buy_matrix = stock_buy(stock_values)
     stock_sell_matrix = stock_sell(stock_buy_matrix).compact
-    # puts stock_buy_matrix
-    # puts stock_sell_matrix
-    sell = []
-    sell << stock_sell_matrix.reduce() { |acc, item| item > acc ? acc = item : acc }
-    sell
+    sell_value = []
+    sell_value << stock_sell_matrix.reduce() { |acc, item| item > acc ? acc = item : acc }
+    buy_day = stock_sell_matrix.index(sell_value[0])
+    sell_day = stock_buy_matrix[buy_day].index(sell_value[0]) + buy_day
+
+    buy_sell = [buy_day, sell_day]
 end
 
 def stock_buy(stock_values)
     stock_matrix = {}
     stock_values.each_with_index do |value, index|
-        remaining_stock = stock_values.slice(index + 1, stock_values.count)
+        remaining_stock = stock_values.slice(index, stock_values.count)
         capital_gains = []
         remaining_stock.each do |num|
             capital_gains << num - value
@@ -33,4 +34,4 @@ end
 
 
 
-puts stock_picker([17,3,6,9,15,8,6,1,10])
+p stock_picker([17,3,6,9,15,8,6,1,10])
