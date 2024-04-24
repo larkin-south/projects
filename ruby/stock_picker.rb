d2c1014b90de4f1d35550811c07260b4cacccdf4
Larkin South <160358440+larkin-south@users.noreply.github.com>
2024-04-23 19:59:38 -0500

    Finishing final calculations for stock_picker.

--- ruby/stock_picker.rb
+++ ruby/stock_picker.rb
@@ -1,13 +1,36 @@
 def stock_picker(stock_values)
+    stock_buy_matrix = stock_buy(stock_values)
+    stock_sell_matrix = stock_sell(stock_buy_matrix).compact
+    # puts stock_buy_matrix
+    # puts stock_sell_matrix
+    sell = []
+    sell << stock_sell_matrix.reduce() { |acc, item| item > acc ? acc = item : acc }
+    sell
+end
 
+def stock_buy(stock_values)
+    stock_matrix = {}
+    stock_values.each_with_index do |value, index|
+        remaining_stock = stock_values.slice(index + 1, stock_values.count)
+        capital_gains = []
+        remaining_stock.each do |num|
+            capital_gains << num - value
+        end
+        stock_matrix[index] = capital_gains
+    end
+    stock_matrix
+end
+
+def stock_sell(stock_buy_matrix)
+    best_gains = []
+    stock_buy_matrix.each_with_index do |value, index|
+        best_gains << stock_buy_matrix[index].reduce() do |acc, item| 
+            item > acc ? acc = item : acc
+        end
+    end
+    best_gains
 end
 
 
 
-
-
-stock_picker([17,3,6,9,15,8,6,1,10])
-
-# for each num in the array
-#     slice the array up to the current index
-#     return hash of current num as key with value equal to num subtracted from remaining items
+puts stock_picker([17,3,6,9,15,8,6,1,10])
