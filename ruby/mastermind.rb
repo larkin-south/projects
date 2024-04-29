09368ee4a79770a6707db01433118841a577e7b9
Larkin South <160358440+larkin-south@users.noreply.github.com>
2024-04-28 22:00:01 -0500

    Mastermind pseudocode added.

---
+++ ruby/mastermind.rb
@@ -0,0 +1,12 @@
+# game starts with computer randomly assinging colors to array
+# player guesses assortment of colors in an order
+# computer compares input to its own array
+# if any colors match, then check for order matching as well
+# each correct item and position gets black peg from computer
+# right color, wrong position gets white peg
+# no pegs returned if no color in guess is used in code
+
+# player guesses list of colors into array
+# compare to compter array, any matching values AND index returns black
+# matching value ONLY returns white
+# when arrays match, win
