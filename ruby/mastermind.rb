9562aa09d323c33a4253f233726433a94b31e1f8
Larkin South <160358440+larkin-south@users.noreply.github.com>
2024-04-29 11:59:31 -0500

    Working on getting user input for Mastermind.

--- ruby/mastermind.rb
+++ ruby/mastermind.rb
@@ -10,3 +10,40 @@
 # compare to compter array, any matching values AND index returns black
 # matching value ONLY returns white
 # when arrays match, win
+
+# class GameEnvironment
+#   attr_reader :color_options
+
+#   def self.choose_colors
+#     @color_options = %w[red yellow blue green black white]
+#     @color_options.sample(4)
+#   end
+# end
+
+class Codemaker
+
+  def choose_colors
+    @color_options = %w[red yellow green blue black white]
+    @color_options.sample(4)
+  end
+
+end
+
+class Codebreaker
+
+  def new_guess
+    puts 'Enter 4 numbers matching with the color options shown:'
+    puts 'Red - 1 | Yellow - 2 | Green - 3 | Blue - 4 | Black - 5 | White - 6'
+    guess = []
+    while guess.count != 4
+      unless guess.between?(1, 6) && guess.instance_of?(Integer) && guess.count == 4
+        guess.push(gets.chomp)
+      end
+    end
+  end
+
+end
+
+codemaker = Codemaker.new
+codebreaker = Codebreaker.new
+p codemaker.choose_colors
