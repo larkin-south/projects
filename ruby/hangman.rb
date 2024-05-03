ee51f52a6ab5fff15a866718c6190bbc6fae89bd
Larkin South <160358440+larkin-south@users.noreply.github.com>
2024-05-03 13:34:29 -0500

    Working on top-level logic for game to function.

--- ruby/hangman.rb
+++ ruby/hangman.rb
@@ -5,7 +5,6 @@ module GameSetup
     word_list = word_list.lines.filter do |word|
       word.length.between?(5, 12)
     end
-
     word_list.sample
   end
 
@@ -16,11 +15,16 @@ end
 
 module Gameplay
   def get_player_guess
+    puts 'Enter a letter:'
     loop do
       input = gets.chomp
       break if input.match?(/[a-zA-Z]/) && input.length == 1 && !player.guessed_letters.any?(input)
     end
     input
+  end
+
+  def update_attempts
+    player.attempts += 1
   end
 
   def is_guess_correct?(word, guess)
@@ -29,21 +33,51 @@ module Gameplay
 
   def correct_guess(guess)
     player.guessed_letters.push(guess)
+
+    # return False if player.word.split('').&(guess.split('')).count < 1
+
+    letter_index = []
+    player.word.split('').each_with_index do |letter, index|
+      letter_index.push(index) if letter == guess
+    end
+
+    letter_index.each do |index|
+      player.word_board[index] = guess
+    end
+  end
+
+  # def wrong_guess(guess)
+    
+  # end
+
+  def win?
+    player.word == player.word_board
   end
 end
 
 class PlayerGameData
-  attr_reader :word, :word_board, :guessed_letters
+  attr_accessor :attempts
+  attr_reader :word, :word_board, :guessed_letters, :guess_limit
 
   include GameSetup
+  include Gameplay
 
   def initialize
     @word = pick_word_from_list
     @word_board = create_word_board(@word)
     @guessed_letters = []
+    @guess_limit = 15
+    @attempts = 0
   end
 end
 
+player = PlayerGameData.new
+player_guess = player.get_player_guess
+player.update_attempts
+player.is_guess_correct? ? correct_guess(player_guess) : player.guessed_letters.push(guess)
+if player.win?
+  puts "You win!"
+end
 
 get letter guess from user
 add letter to guessed_letters and update word board with letter if match
