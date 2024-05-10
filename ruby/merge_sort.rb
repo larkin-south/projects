5b5243c6e789d6b3e8af8ce2a90ae3c45224d626
Larkin South <160358440+larkin-south@users.noreply.github.com>
2024-05-10 16:11:41 -0500

    Working on merge sort.

--- ruby/merge_sort.rb
+++ ruby/merge_sort.rb
@@ -1,30 +1,50 @@
 def merge(array)
-  first_half = array.slice!(0, array.length / 2)
-  if first_half.length > 1
-    new_array = merge(first_half)
-  else
-    new_array = []
-  end
+  array1 = array.slice!(0, array.length / 2)
+  array2 = array
+  array1 = merge(array1) unless array1.length <= 1
+  array2 = merge(array2) unless array2.length <= 1
 
-  if array.length > 1
-    array = merge(array)
-  else
-    array = []
-  end
+  # return array1 if array1.length == 1
+  # return array2 if array2.length == 1
 
-
-  until first_half.empty? || array.empty?
-    if first_half[0] <= array.flatten[0]
-      new_array << first_half[0]
-      first_half.shift
-      break
+  new_array = []
+  a = 0
+  b = 0
+  until a == array1.length || b == array2.length do
+    if array1[a] < array2[b]
+      new_array << array1[a]
+      a += 1
     else
-      new_array << array[0]
-      array.shift
+      new_array << array2[b]
+      b += 1
     end
   end
 
-  new_array << (first_half.empty? ? array : first_half)
+  if array1[a].nil?
+    new_array << array2[b..(array2.length - 1)]
+  elsif array2[b].nil?
+    new_array << array1[a..(array1.length - 1)]
+  end
+
+  # if array.length > 1
+  #   array = merge(array)
+  # else
+  #   array = []
+  # end
+
+
+  # until first_half.empty? || array.empty?
+  #   if first_half[0] <= array.flatten[0]
+  #     new_array << first_half[0]
+  #     first_half.shift
+  #     break
+  #   else
+  #     new_array << array[0]
+  #     array.shift
+  #   end
+  # end
+
+  # new_array << (first_half.empty? ? array : first_half)
 
 
 
@@ -44,5 +64,10 @@ end
 # else
 #   return value
 # end
+# 
+# 4 1 2 3
+# 4 1     2 3
+# 4   1       2   3
+# 
 
 print merge([3, 1, 4, 2, 2, 2, 1, 3])
