3d5e5280e9d8782cbf755397315d1adc56a6d137
Larkin South <160358440+larkin-south@users.noreply.github.com>
2024-05-13 21:48:39 -0500

    Working on LinkList methods.

--- ruby/linked_list.rb
+++ ruby/linked_list.rb
@@ -1,18 +1,21 @@
 class LinkedList 
   attr_accessor :head, :tail
 
-  def initialize
-    @head = nil
+  def initialize(value = nil)
+    @head = @tail = Node.new(value)
   end
 
-  def append(value)
-    @tail.next_node = value
-    value.next_node = nil
+  def append(value = nil, next_node = nil)
+    node = Node.new(value, next_node)
+    node.next_node = nil
+    @tail.next_node = node.value
+    @tail = node
   end
 
-  def prepend(value)
-    value.next_node = @head
-    @head = value
+  def prepend(value, next_node)
+    node = Node.new(value, next_node)
+    node.next_node = @head.value
+    @head = node
   end
 
   def size
@@ -25,14 +28,25 @@ class LinkedList
     count
   end
 
+  # def pop
+
+  # end
 
 end
 
 class Node 
   attr_accessor :value, :next_node
 
-  def initialize
-    @value = nil
-    @next_node = nil
+  def initialize(value = nil, next_node = nil)
+    @value = value
+    @next_node = next_node
   end
 end
+
+list = LinkedList.new("test")
+# list.append("another test")
+p list.head.value
+p list.head.next_node
+p list.tail
+# p list.tail.next_node
+# list[0,1]
