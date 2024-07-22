b69c99a89aa1f14a93aa38ff9aa1d9f1dae07407
Larkin South <160358440+larkin-south@users.noreply.github.com>
2024-07-21 21:15:47 -0500

    rspec 14 complete

--- ruby/rspec_testing/lib/14_find_number.rb
+++ ruby/rspec_testing/lib/14_find_number.rb
@@ -11,4 +11,17 @@ class FindNumber
     @answer = answer.value
     @guess = guess
   end
+
+  def make_guess
+    ((@min + @max) / 2).floor
+  end
+
+  def game_over?
+    @answer == @guess
+  end
+
+  def update_range
+    return @min = @guess + 1 if @guess < @answer
+    return @max = @guess - 1 if @guess > @answer
+  end
 end
