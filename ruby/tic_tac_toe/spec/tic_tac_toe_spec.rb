f4dc6dfbb7c21926e2a5dab8c376208eeef7b539
Larkin South <160358440+larkin-south@users.noreply.github.com>
2024-08-06 16:39:51 -0500

    worked on tictactoe and created connect4 directory and files

--- ruby/tic_tac_toe/spec/tic_tac_toe_spec.rb
+++ ruby/tic_tac_toe/spec/tic_tac_toe_spec.rb
@@ -3,25 +3,37 @@ require_relative '../test_lib_markfla/game.rb'
 require_relative '../test_lib_markfla/board.rb'
 
 describe Game do
-  # describe '#game_loop' do
-  #   subject(:game) { described_class.new('test1', 'test2') }
-  #   # allow(game_loop).to receive(:gets).and_return(true)
-  #   # allow(game_loop).to receive(:puts)
+  describe '#game_loop' do
+    subject(:game) { described_class.new('test1', 'test2') }
+    let(:board) { instance_double(Board.new) }
 
-  #   context '@game_over is true' do
-  #     # allow(game).to receive(:gets).and_return(true)
-  #     before do
-  #       allow(game).to receive(:chomp).twice
-  #     end
+    describe '#make_move' do
+      context 'when move is valid' do
+        before do
+          position = double
+          expect(game).to receive(:gets).and_return(position)
+          expect(position).to receive(:chomp).and_return(2)
+          expect(position).to receive(:to_i)
+          allow(board).to receive(:valid_move?).and_return(true)
+        end
+        it 'will call #update_position' do
+          # game.stub(:gets).and_return(2)
+          # game.stub(:chomp).and_return(2)
+          expect(board).to receive(:valid_move?).with(2).and_return(true)
+          expect(game).to receive(:update_position).with(2, 'X')
+          game.make_move
+        end
+      end
+    end
+  end
 
-
-  #     it 'stops loop and puts message' do
-  #       expect(game).to receive(:chomp)
-  #       expect(game).to receive(:gets).and_return(true)
-  #       game.game_loop
-  #     end
-  #   end
-  # end
+  describe '#swap_player' do
+    context 'when players swap' do
+      it 'changes p1 to p2' do
+        
+      end
+    end
+  end
 end
 
 describe Board do
@@ -34,6 +46,15 @@ describe Board do
       end
       it 'returns true' do
         expect(board.valid_move?(2)).to eq(true)
+      end
+    end
+
+    context 'number outside 0-8 not valid' do
+      before do
+        allow(board).to receive(:gets).and_return('-')
+      end
+      it 'returns true' do
+        expect(board.valid_move?(9)).to eq(false)
       end
     end
   end
